f826872cc50f9c7af5a16c86260ecd5062931498
Hamza El makhroubi <hel-makh@e2r8p3.1337.ma>
2022-04-24 17:17:50 +0000

    changed cout text

--- CPP Module 01/ex00/Zombie.cpp
+++ CPP Module 01/ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 17:04:29 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/04/23 17:12:13 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/04/24 17:13:21 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ Zombie::Zombie(std::string zombieName) : name(zombieName) {
 }
 
 Zombie::~Zombie(void) {
-    std::cout << this->name << std::endl;
+    std::cout << this->name << ": ate too much BraiiiiiiinnnzzzZ and died..." << std::endl;
 }
 
 void    Zombie::announce(void) {
--- CPP Module 01/ex01/Zombie.cpp
+++ CPP Module 01/ex01/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 17:04:29 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/04/23 18:50:39 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/04/24 17:13:35 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ Zombie::Zombie(void) {
 }
 
 Zombie::~Zombie(void) {
-	std::cout << this->name << std::endl;
+    std::cout << this->name << ": ate too much BraiiiiiiinnnzzzZ and died..." << std::endl;
 }
 
 void	Zombie::announce(void) {

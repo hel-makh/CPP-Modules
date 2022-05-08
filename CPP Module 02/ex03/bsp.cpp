fba824bb12dd95f1a5c2bc1ff83483522984bcdf
Hamza El makhroubi <hel-makh@e2r8p3.1337.ma>
2022-05-08 12:16:38 +0100

    fixed vertex point or in edge

--- CPP Module 02/ex03/bsp.cpp
+++ CPP Module 02/ex03/bsp.cpp
@@ -20,8 +20,9 @@ static Fixed	absFixed(Fixed n) {
 	return (n);
 }
 
-static Fixed	calculateArea(Point const a, Point const b, Point const c) {
-	Fixed	Area;
+static bool	calculateArea(
+	Point const a, Point const b, Point const c, Fixed & Area
+	) {
 	Fixed	trapAreas[3];
 	
 	trapAreas[0] = a.getX() * (b.getY() - c.getY());
@@ -29,18 +30,24 @@ static Fixed	calculateArea(Point const a, Point const b, Point const c) {
 	trapAreas[2] = c.getX() * (a.getY() - b.getY());
 
 	Area = absFixed((trapAreas[0] + trapAreas[1] + trapAreas[2]) / Fixed(2.0f));
+
+	if (trapAreas[0] == Fixed(0.0f)
+		|| trapAreas[1] == Fixed(0.0f)
+		|| trapAreas[2] == Fixed(0.0f))
+		return (false);
 	
-	return (Area);
+	return (true);
 }
 
 bool	bsp(Point const a, Point const b, Point const c, Point const point) {
 	Fixed	triangleArea;
 	Fixed	triangleAreas[3];
 
-	triangleArea = calculateArea(a, b, c);
-	triangleAreas[0] = calculateArea(a, b, point);
-	triangleAreas[1] = calculateArea(a, point, c);
-	triangleAreas[2] = calculateArea(point, b, c);
+	if (!calculateArea(a, b, c, triangleArea)
+		|| !calculateArea(a, b, point, triangleAreas[0])
+		|| !calculateArea(a, point, c, triangleAreas[1])
+		|| !calculateArea(point, b, c, triangleAreas[2]))
+		return (false);
 
 	if (triangleArea == triangleAreas[0] + triangleAreas[1] + triangleAreas[2])
 		return (true);

092019c32f2377895f2fdcb12ca18bd989f719f1
Hamza El makhroubi <hel-makh@e2r8p5.1337.ma>
2022-05-10 14:49:40 +0100

    fixed error when s1 == s2

--- CPP Module 01/ex04/strReplace.cpp
+++ CPP Module 01/ex04/strReplace.cpp
@@ -6,21 +6,22 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/24 18:31:05 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/05/10 13:47:38 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/05/10 14:49:20 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "strReplace.hpp"
 
-int	strReplace(std::string& str, char* s1, char* s2) {
-	size_t	s1_pos;
+void	strReplace(std::string& str, char* s1, char* s2) {
+	size_t	occurrence;
 
 	if (!s1 || !s1[0])
-		return (0);
-	s1_pos = str.find(s1);
-	if (s1_pos == std::string::npos)
-		return (0);
-	str.erase(s1_pos, strlen(s1));
-	str.insert(s1_pos, s2);
-	return (1);
+		return ;
+	occurrence = 0;
+	while ((occurrence = str.find(s1, occurrence)) != std::string::npos)
+	{
+		str.erase(occurrence, strlen(s1));
+		str.insert(occurrence, s2);
+		occurrence += strlen(s2);
+	}
 }

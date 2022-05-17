cb952985eabfed422ea3851b1c0e71cadf1f1a6f
Hamza El makhroubi <hel-makh@e2r8p3.1337.ma>
2022-05-17 12:40:13 +0100

    fixed error

--- CPP Module 02/ex03/bsp.cpp
+++ CPP Module 02/ex03/bsp.cpp
@@ -20,9 +20,8 @@ static Fixed	absFixed(Fixed n) {
 	return (n);
 }
 
-static bool	calculateArea(
-	Point const a, Point const b, Point const c, Fixed & Area
-	) {
+static Fixed	calculateArea(Point const a, Point const b, Point const c) {
+	Fixed	Area;
 	Fixed	trapAreas[3];
 	
 	trapAreas[0] = a.getX() * (b.getY() - c.getY());
@@ -30,23 +29,22 @@ static bool	calculateArea(
 	trapAreas[2] = c.getX() * (a.getY() - b.getY());
 
 	Area = absFixed((trapAreas[0] + trapAreas[1] + trapAreas[2]) / Fixed(2.0f));
-
-	if (trapAreas[0] == Fixed(0.0f)
-		|| trapAreas[1] == Fixed(0.0f)
-		|| trapAreas[2] == Fixed(0.0f))
-		return (false);
 	
-	return (true);
+	return (Area);
 }
 
 bool	bsp(Point const a, Point const b, Point const c, Point const point) {
 	Fixed	triangleArea;
 	Fixed	triangleAreas[3];
 
-	if (!calculateArea(a, b, c, triangleArea)
-		|| !calculateArea(a, b, point, triangleAreas[0])
-		|| !calculateArea(a, point, c, triangleAreas[1])
-		|| !calculateArea(point, b, c, triangleAreas[2]))
+	triangleArea = calculateArea(a, b, c);
+	triangleAreas[0] = calculateArea(a, b, point);
+	triangleAreas[1] = calculateArea(a, point, c);
+	triangleAreas[2] = calculateArea(point, b, c);
+	
+	if (triangleAreas[0] == Fixed(0.0f)
+		|| triangleAreas[1] == Fixed(0.0f)
+		|| triangleAreas[2] == Fixed(0.0f))
 		return (false);
 
 	if (triangleArea == triangleAreas[0] + triangleAreas[1] + triangleAreas[2])

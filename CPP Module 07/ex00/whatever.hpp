d1288aa4189aa19534b6454abd679eb7b31e3b8b
Hamza El makhroubi <hel-makh@e2r7p4.1337.ma>
2022-05-28 16:48:33 +0100

    cpp07 ex00 fixed sign

--- CPP Module 07/ex00/whatever.hpp
+++ CPP Module 07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 16:41:16 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/05/28 16:45:57 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/05/28 16:48:15 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,12 @@ void	swap(T & v1, T & v2) {
 
 template <typename T>
 T const & min(T const & v1, T const & v2) {
-	return (v1 <= v2 ? v1 : v2);
+	return (v1 < v2 ? v1 : v2);
 }
 
 template <typename T>
 T const & max(T const & v1, T const & v2) {
-	return (v1 >= v2 ? v1 : v2);
+	return (v1 > v2 ? v1 : v2);
 }
 
 #endif
